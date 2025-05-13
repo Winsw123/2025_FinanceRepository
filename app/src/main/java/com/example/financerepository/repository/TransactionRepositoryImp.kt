6f784f46672f3e272c508748d6286d6a2758cf68
洪筱晴 <tiffany.hung@George-HungdeMacBook-Pro.local>
2025-05-13 12:15:23 +0800

    TransactionRepository

---
+++ app/src/main/java/com/example/financerepository/repository/TransactionRepositoryImp.kt
@@ -0,0 +1,30 @@
+package com.example.financerepository.repository
+
+import com.example.financerepository.data.dao.TransactionDao
+import com.example.financerepository.data.model.Transaction
+import kotlinx.coroutines.flow.Flow
+
+class TransactionRepositoryImpl(
+    private val dao: TransactionDao
+) : TransactionRepository {
+
+    // add
+    override suspend fun insertTransaction(transaction: Transaction) {
+        dao.insertTransaction(transaction)
+    }
+
+    // delete
+    override suspend fun deleteTransaction(transaction: Transaction) {
+        dao.deleteTransaction(transaction)
+    }
+
+    // update
+    override suspend fun updateTransaction(transaction: Transaction) {
+        dao.updateTransaction(transaction)
+    }
+
+    // get transactions
+    override fun getAllTransactions(): Flow<List<Transaction>> {
+        return dao.getAllTransactions()
+    }
+}
